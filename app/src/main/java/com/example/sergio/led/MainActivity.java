bdd8fee483c912b82b291e613fdaab3e05c00cd8
sergio <sergio.cordedda@abinsula.com>
2017-10-31 15:23:50 +0100

    Add color picker to main activity

--- app/src/main/java/com/example/sergio/led/MainActivity.java
+++ app/src/main/java/com/example/sergio/led/MainActivity.java
@@ -1,22 +1,115 @@
 package com.example.sergio.led;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
+import android.widget.Toast;
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorChangedListener;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View root;
+    private int currentBackgroundColor = 0xffffffff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+/*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+*/
+
+
+        findViewById(R.id.btn_dialog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Context context = MainActivity.this;
+
+                ColorPickerDialogBuilder
+                        .with(context)
+                        .setTitle("Color Picker")
+                        .initialColor(currentBackgroundColor)
+                        .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                        .density(12)
+                        .setOnColorChangedListener(new OnColorChangedListener() {
+                            @Override
+                            public void onColorChanged(int selectedColor) {
+                                // Handle on color change
+                                Log.d("ColorPicker", "onColorChanged: 0x" + Integer.toHexString(selectedColor));
+                            }
+                        })
+                        .setOnColorSelectedListener(new OnColorSelectedListener() {
+                            @Override
+                            public void onColorSelected(int selectedColor) {
+                                toast("onColorSelected: 0x" + Integer.toHexString(selectedColor));
+                            }
+                        })
+                        .setPositiveButton("ok", new ColorPickerClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                                changeBackgroundColor(selectedColor);
+                                if (allColors != null) {
+                                    StringBuilder sb = null;
+
+                                    for (Integer color : allColors) {
+                                        if (color == null)
+                                            continue;
+                                        if (sb == null)
+                                            sb = new StringBuilder("Color List:");
+                                        sb.append("\r\n#" + Integer.toHexString(color).toUpperCase());
+                                    }
+
+                                    if (sb != null)
+                                        Toast.makeText(getApplicationContext(), sb.toString(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .showColorEdit(true)
+                        .setColorEditTextColor(ContextCompat.getColor(MainActivity.this, android.R.color.holo_blue_bright))
+                        .build()
+                        .show();
+            }
+        });
+
     }
 
     public void onClick(View view)
     {
         startActivity(new Intent(MainActivity.this, ColorPicker.class));
     }
+
+    private void changeBackgroundColor(int selectedColor) {
+        currentBackgroundColor = selectedColor;
+        root.setBackgroundColor(selectedColor);
+    }
+
+    private void toast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
 }
